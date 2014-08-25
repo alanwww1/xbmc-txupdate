0ef154cbf0db1f881fd8a0dc81586ecd3551af8a
alanwww1 <attila.jakosa@gmail.com>
2014-08-25 23:01:05 +0200

    added: ability to force creation of updateTX files

--- lib/ProjectHandler.cpp
+++ lib/ProjectHandler.cpp
@@ -230,12 +230,16 @@ bool CProjectHandler::CreateMergedResources()
                  pPOEntryTX->msgID == pcurrPOEntryEN->msgID)
         {
           mergedPOHandler.AddNumPOEntryByID(numID, *pPOEntryTX, *pcurrPOEntryEN, true);
+          if (g_Settings.GetForceTXUpdate())
+            updTXPOHandler.AddNumPOEntryByID(numID, *pPOEntryTX, *pcurrPOEntryEN, true);
         }
         //2. Tx entry plural
         else if (pPOEntryTX && !pcurrPOEntryEN->msgIDPlur.empty() && !pPOEntryTX->msgStrPlural.empty() &&
                  pPOEntryTX->msgIDPlur == pcurrPOEntryEN->msgIDPlur)
         {
           mergedPOHandler.AddNumPOEntryByID(numID, *pPOEntryTX, *pcurrPOEntryEN, true);
+          if (g_Settings.GetForceTXUpdate())
+            updTXPOHandler.AddNumPOEntryByID(numID, *pPOEntryTX, *pcurrPOEntryEN, true);
         }
         //3. Upstr entry single
         else if (pPOEntryUpstr && pcurrPOEntryEN->msgIDPlur.empty() && !pPOEntryUpstr->msgStr.empty() &&
@@ -276,9 +280,17 @@ bool CProjectHandler::CreateMergedResources()
           updTXPOHandler.AddClassicEntry(*pcurrPOEntryEN, *pcurrPOEntryEN, true);
         }
         else if (pPOEntryTX && pcurrPOEntryEN->msgIDPlur.empty() && !pPOEntryTX->msgStr.empty()) // Tx entry single
+        {
           mergedPOHandler.AddClassicEntry(*pPOEntryTX, *pcurrPOEntryEN, true);
+          if (g_Settings.GetForceTXUpdate())
+            updTXPOHandler.AddClassicEntry(*pPOEntryTX, *pcurrPOEntryEN, true);
+        }
         else if (pPOEntryTX && !pcurrPOEntryEN->msgIDPlur.empty() && !pPOEntryTX->msgStrPlural.empty()) // Tx entry plural
+        {
           mergedPOHandler.AddClassicEntry(*pPOEntryTX, *pcurrPOEntryEN, true);
+          if (g_Settings.GetForceTXUpdate())
+            updTXPOHandler.AddClassicEntry(*pPOEntryTX, *pcurrPOEntryEN, true);
+        }
         else if (pPOEntryUpstr && pcurrPOEntryEN->msgIDPlur.empty() && !pPOEntryUpstr->msgStr.empty()) // Upstr entry single
         {
           mergedPOHandler.AddClassicEntry(*pPOEntryUpstr, *pcurrPOEntryEN, true);
@@ -304,7 +316,8 @@ bool CProjectHandler::CreateMergedResources()
       }
 
       if ((updTXPOHandler.GetNumEntriesCount() !=0 || updTXPOHandler.GetClassEntriesCount() !=0) &&
-        (strLangCode != "en" || !g_HTTPHandler.ComparePOFilesInMem(&updTXPOHandler, pPOHandlerTX, strLangCode == "en")))
+        (strLangCode != "en" || g_Settings.GetForceTXUpdate() ||
+        !g_HTTPHandler.ComparePOFilesInMem(&updTXPOHandler, pPOHandlerTX, strLangCode == "en")))
       {
         updTXPOHandler.SetPreHeader(strResPreHeader);
         updTXPOHandler.SetHeaderNEW(*itlang);
